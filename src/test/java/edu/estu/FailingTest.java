65b2ad81770b1f5a2aa7007a63acc8c16024702a
bariskilinc6 <131043201+bariskilinc6@users.noreply.github.com>
2024-01-06 22:37:06 +0300

    Update FailingTest.java

--- src/test/java/edu/estu/FailingTest.java
+++ src/test/java/edu/estu/FailingTest.java
@@ -13,5 +13,23 @@ import static org.junit.Assert.assertTrue;
  * Unit tests for the first part of the project.
  */
 public class FailingTest {
+      public class UserLoginTest {
 
+    // Testing a deliberate scenario where the user login method intentionally goes into an infinite loop
+    @Test(timeout = 1000) // Setting a timeout to prevent the infinite loop
+    public void testInfiniteLoopOnUserLogin() {
+        UserLoginService loginService = new UserLoginService();
+
+        try {
+            // Invoking the method that enters an infinite loop
+            loginService.infiniteUserLogin();
+        } catch (Throwable expected) {
+            // If an infinite loop error is caught, the test is considered successful
+            return;
+        }
+
+        // If no infinite loop error is caught within the specified time, the test fails
+        throw new AssertionError("No infinite loop error occurred!");
+    }
+}
 }
